0448fbed666fe57ff770a3bde4654ed4b2448195
iburaksag <lfgmhal97>
2023-05-19 03:44:55 +0100

    Footer component is completed.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin } from "react-icons/bs";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <a href="#" className="footer__logo">
+        BURAK SAG
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.instagram.com/iburaksag" target="_blank">
+          <FiInstagram />
+        </a>
+        <a href="https://www.twitter.com/iburaks" target="_blank">
+          <IoLogoTwitter />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/burak-sag-4883a2139/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>
+          2023 &copy; Burak Sag Personal Portfolio. All rights reserved.
+        </small>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer
